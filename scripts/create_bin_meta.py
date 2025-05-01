db014a522b5f4f47c164e9aee991dbba2b0a9b41
Kestin Goforth <kgoforth1503@gmail.com>
2025-05-01 15:07:29 -0400

    Check for duplicates

--- scripts/create_bin_meta.py
+++ scripts/create_bin_meta.py
@@ -46,15 +46,32 @@ def main(src, outfile):
         for e in bins
     ]
 
-    bin_dict = { e["file_name"]: e for e in bins }
-
     if outfile:
+        bin_dict = {e["file_name"]: e for e in bins}
         if outdir := os.path.split(outfile)[0]:
             os.makedirs(outdir, exist_ok=True)
         with open(outfile, "w+", encoding="utf-8") as out:
             yaml.dump(bin_dict, out)
     else:
-        click.echo(json.dumps([asdict(e) for e in bin_dict], indent=2))
+        click.echo(json.dumps([e for e in bins], indent=2))
+
+    # Check for duplicates using the file checksum
+    checksums = {}
+    for info in bins:
+        checksum = info["file_checksum"]
+        if checksum in checksums:
+            checksums[checksum] += [info["file_name"]]
+        else:
+            checksums[checksum] = [info["file_name"]]
+    duplicates = [k for k, v in checksums.items() if len(v) > 1]
+    if duplicates:
+        click.echo("Duplicate files found:")
+        for checksum in duplicates:
+            click.echo(f"  {checksum}:")
+            for fn in checksums[checksum]:
+                click.echo(f"    {fn}")
+    else:
+        click.echo("No duplicate files found.")
 
     click.echo("Done.")
 
