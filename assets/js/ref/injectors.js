e9bc9c76c3f3471679fd645bc445f825cf9a62dc
Kestin Goforth <kgoforth1503@gmail.com>
2025-05-06 12:12:58 -0400

    Move injectors.js to ref folder

---
+++ assets/js/ref/injectors.js
@@ -0,0 +1,35 @@
+const INJECTOR_CONFIGS = [
+  {
+    title: "B5254T4 Greens / Volvo 9202100 / Bosch 0-280-155-968",
+    krkte: 0.07253,
+    kvb: 446.70,
+    temin: 0.7008,
+    teminva: 0.4000,
+    tvub: [1.8144, 1.2000, 0.7648, 0.4992, 0.3808],
+  },
+
+  {
+    title: "B5244T5 Blues / Volvo 9186060 / Bosch 0-280-155-830",
+    krkte: 0.08213,
+    kvb: 368.40,
+    temin: 0.7008,
+    teminva: 0.4000,
+    tvub: [1.8144, 1.2000, 0.7648, 0.4992, 0.3808],
+  },
+  {
+    title: "B5234T3 Blues / Volvo 9186060 / Bosch 0-280-155-830",
+    krkte: 0.0800,
+    kvb: 368.40,
+    temin: 0.70133,
+    teminva: 0.70133,
+    tvub: [2.1888, 1.4784, 1.0560, 0.8064, 0.6144],
+  },
+  {
+    title: "B5254T2 Browns / Volvo 9186340 / Bosch 0-280-155-831",
+    krkte: 0.09493,
+    kvb: 341.80,
+    temin: 0.7008,
+    teminva: 0.7136,
+    tvub: [1.9584, 1.2672, 0.8992, 0.6624, 0.5024],
+  },
+]
